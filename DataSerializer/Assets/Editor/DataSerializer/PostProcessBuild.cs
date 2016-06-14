f5d12cbeb891669f11398203f8c0d2eb5fd8cd87
tkyaji <tkyaji@gmail.com>
2016-06-14 17:33:05 +0900

    bugfix

--- DataSerializer/Assets/Editor/DataSerializer/PostProcessBuild.cs
+++ DataSerializer/Assets/Editor/DataSerializer/PostProcessBuild.cs
@@ -18,12 +18,15 @@ public class PostProcessBuild {
 		string projPath = Path.Combine(path, "Unity-iPhone.xcodeproj/project.pbxproj");
 		PBXProject proj = new PBXProject();
 		proj.ReadFromString(File.ReadAllText(projPath));
+		string target = proj.TargetGuidByName("Unity-iPhone");
 
 		string bundleFile = "Frameworks/Plugins/Mac/GZipUtil.bundle";
 		string guid = proj.FindFileGuidByProjectPath(bundleFile);
 		proj.RemoveFile(guid);
 		string bundleFilePath = Path.Combine(path, bundleFile);
 		Directory.Delete(bundleFilePath, true);
+
+		proj.AddFileToBuild(target, proj.AddFile("usr/lib/libz.tbd", "Frameworks/libz.1.2.5.tbd", PBXSourceTree.Sdk));
 
 		File.WriteAllText(projPath, proj.WriteToString());
 	}
