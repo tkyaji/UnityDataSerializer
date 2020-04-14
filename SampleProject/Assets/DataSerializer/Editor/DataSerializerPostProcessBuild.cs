63c5604c39ed450bc9b2a1e3349049dc58d27a9e
tkyaji <tkyaji@gmail.com>
2020-04-14 10:47:33 +0900

    Compatible with Unity2019.3.

--- SampleProject/Assets/DataSerializer/Editor/DataSerializerPostProcessBuild.cs
+++ SampleProject/Assets/DataSerializer/Editor/DataSerializerPostProcessBuild.cs
@@ -12,7 +12,11 @@ public class DataSerializerPostProcessBuild {
 		string projPath = Path.Combine(path, "Unity-iPhone.xcodeproj/project.pbxproj");
 		PBXProject proj = new PBXProject();
 		proj.ReadFromString(File.ReadAllText(projPath));
+#if UNITY_2019_3_OR_NEWER
+		string target = proj.GetUnityFrameworkTargetGuid();
+#else
 		string target = proj.TargetGuidByName("Unity-iPhone");
+#endif
 
 		proj.AddFileToBuild(target, proj.AddFile("usr/lib/libz.tbd", "Frameworks/libz.tbd", PBXSourceTree.Sdk));
 
